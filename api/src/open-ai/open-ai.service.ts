60f07451ff482c13bd3020fa80c8599b67c6ae20
Guillermo Gutierrez <miru.chohi@gmail.com>
2025-03-08 13:35:15 -0500

    no more api key

--- api/src/open-ai/open-ai.service.ts
+++ api/src/open-ai/open-ai.service.ts
@@ -39,18 +39,3 @@ export class OpenAiService {
 }
 
 
-
-
-// const openai = new OpenAI({
-//     apiKey: this.configService.get<string>() "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",
-//   });
-  
-//   const completion = openai.chat.completions.create({
-//     model: "gpt-4o-mini",
-//     store: true,
-//     messages: [
-//       {"role": "user", "content": "write a haiku about ai"},
-//     ],
-//   });
-  
-//   completion.then((result) => console.log(result.choices[0].message));
